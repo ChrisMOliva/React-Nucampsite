98cb4d62cf1d3677c6df1a3e5e49b1f575212a6f
Chris Oliva <chrismoliva@yahoo.com>
2021-05-29 12:55:20 -0700

    Week 4 Workshop

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -4,15 +4,110 @@ import {
   CardImg,
   CardText,
   CardBody,
+  CardTitle,
   Breadcrumb,
   BreadcrumbItem,
+  Button,
+  Modal,
+  ModalHeader,
+  ModalBody,
+  Label,
 } from "reactstrap";
 import { Link } from "react-router-dom";
-
-
-function RenderCampsite({campsite}) {
+import { LocalForm, Control, Errors } from "react-redux-form";
+const required = (val) => val && val.length;
+const maxLength = (len) => (val) => !val || val.length <= len;
+const minLength = (len) => (val) => val && val.length >= len;
+class CommentForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      isModalOpen: false,
+    };
+    this.toggleModal = this.toggleModal.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  toggleModal() {
+    this.setState({
+      isModalOpen: !this.state.isModalOpen,
+    });
+  }
+  handleSubmit(values) {
+    console.log("Current state is: " + JSON.stringify(values));
+    alert("Current state is: " + JSON.stringify(values));
+    this.toggleModal();
+  }
+  render() {
+    return (
+      <div>
+        <Button outline onClick={this.toggleModal}>
+          <i className="fa fa-pencil fa-lg" />
+          Submit Comment
+        </Button>
+        <Modal isOpen={this.state.isModalOpen} toggle={this.toggleModal}>
+          <ModalHeader toggle={this.toggleModal}>Submit Comment</ModalHeader>
+          <ModalBody>
+            <LocalForm onSubmit={(values) => this.handleSubmit(values)}>
+              <Label htmlFor="rating">Rating</Label>
+              <Control.select
+                model=".rating"
+                id="rating"
+                defaultValue={1}
+                name="rating"
+                className="form-control"
+              >
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+              </Control.select>
+              <Label htmlFor="author">Your Name</Label>
+              <Control.text
+                model=".author"
+                id="author"
+                name="author"
+                placeholder="Your Name"
+                className="form-control"
+                validators={{
+                  required,
+                  minLength: minLength(2),
+                  maxLength: maxLength(15),
+                }}
+              />
+              <Errors
+                className="text-danger"
+                model=".author"
+                show="touched"
+                component="div"
+                messages={{
+                  required: "Required",
+                  minLength: "Must be at least 2 characters",
+                  maxLength: "Must be 15 characters or less",
+                }}
+              />
+              <Label htmlFor="text">Comment:</Label>
+              <Control.textarea
+                model=".text"
+                id="text"
+                name="text"
+                placeholder="Type your response..."
+                className="form-control"
+                rows="12"
+              />
+              <Button value="submit" type="submit">
+                Submit Comment
+              </Button>
+            </LocalForm>
+          </ModalBody>
+        </Modal>
+      </div>
+    );
+  }
+}
+function RenderCampsite({ campsite }) {
   return (
-    <div class="col-md-5 m-1">
+    <div className="col-md-5 m-1">
       <Card>
         <CardImg top src={campsite.image} alt={campsite.name} />
         <CardBody>
@@ -22,9 +117,7 @@ function RenderCampsite({campsite}) {
     </div>
   );
 }
-
-function RenderComments({comments}) {
-  console.log(comments);
+function RenderComments({ comments }) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
@@ -32,10 +125,9 @@ function RenderComments({comments}) {
         {comments.map((comment) => {
           return (
             <div key={comment.id}>
+              <p>{comment.text}</p>
+              <p>{comment.author}</p>
               <p>
-                {comment.text}
-                <br />
-                --{comment.author},<span> </span>
                 {new Intl.DateTimeFormat("en-US", {
                   year: "numeric",
                   month: "short",
@@ -45,11 +137,11 @@ function RenderComments({comments}) {
             </div>
           );
         })}
+        <CommentForm />
       </div>
     );
   }
 }
-
 function CampsiteInfo(props) {
   if (props.campsite) {
     return (
@@ -65,16 +157,13 @@ function CampsiteInfo(props) {
             <h2>{props.campsite.name}</h2>
             <hr />
           </div>
-        </div>
-        <div className="row">
-          <RenderCampsite campsite={props.campsite} />
-          <RenderComments comments={props.comments} />
+          <div className="row">
+            <RenderCampsite campsite={props.campsite} />
+            <RenderComments comments={props.comments} />
+          </div>
         </div>
       </div>
     );
   }
-  return <div />;
 }
-
-
 export default CampsiteInfo;
