385711398df69e1e1918ae7f0b0def57d504324c
Chris Oliva <chrismoliva@yahoo.com>
2021-06-03 14:44:57 -0700

    Redux Actions

--- src/components/CampsiteInfoComponent.js
+++ src/components/CampsiteInfoComponent.js
@@ -33,9 +33,8 @@ class CommentForm extends React.Component {
     });
   }
   handleSubmit(values) {
-    console.log("Current state is: " + JSON.stringify(values));
-    alert("Current state is: " + JSON.stringify(values));
     this.toggleModal();
+    this.props.addComment(this.props.campsiteId, values.rating, values.author, values.text);
   }
   render() {
     return (
@@ -117,7 +116,7 @@ function RenderCampsite({ campsite }) {
     </div>
   );
 }
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, campsiteId }) {
   if (comments) {
     return (
       <div className="col-md-5 m-1">
@@ -137,7 +136,7 @@ function RenderComments({ comments }) {
             </div>
           );
         })}
-        <CommentForm />
+        <CommentForm campsiteId={campsiteId} addComment={addComment} />
       </div>
     );
   }
@@ -159,7 +158,11 @@ function CampsiteInfo(props) {
           </div>
           <div className="row">
             <RenderCampsite campsite={props.campsite} />
-            <RenderComments comments={props.comments} />
+            <RenderComments 
+              comments={props.comments} 
+              addComment={props.addComment}
+              campsiteId={props.campsite.id}
+              />
           </div>
         </div>
       </div>
